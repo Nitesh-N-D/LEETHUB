ab70ca8d4e2832b97db00550997b9affd6f51966
Nitesh N D <niteshdwaraka@gmail.com>
2025-08-31 16:08:27 +0530

    Update 0070-climbing-stairs.cpp

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2) return n; 
+        if(n<=2)
+            return n; 
 
-        int first = 1, second = 2, ways = 0;
-        for (int i = 3; i <= n; i++) {
-            ways = first + second;
-            first = second;
-            second = ways;
+        int first=1,second=2,ways=0;
+        for (int i=3;i<=n;i++) {
+            ways=first+second;
+            first=second;
+            second=ways;
         }
         return second;
     }
