0fa1f4b65c561d017ae24330ab86d2dca1b4df74
Nitesh N D <niteshdwaraka@gmail.com>
2025-09-05 17:05:40 +0530

    Time: 0 ms (100%), Space: 8.1 MB (82.99%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        int n=s.length();
+        int res=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]!=' ')
+            {
+                while(i<n&&s[i]!=' ')
+                i++;
+                res++;
+            }
+        }
+        return res;
+        
+    }
+};
