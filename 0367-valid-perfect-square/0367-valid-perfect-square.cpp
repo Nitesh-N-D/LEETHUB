48e07be4efa871ebcec0a3c58c91a95e3f2a9540
Nitesh N D <niteshdwaraka@gmail.com>
2025-09-06 21:06:51 +0530

    Time: 0 ms (100%), Space: 7.8 MB (17.91%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int n=1;
+        while(num>0)
+        {
+            num=num-n;
+            n=n+2;
+        }
+        return num==0;
+        
+    }
+};
